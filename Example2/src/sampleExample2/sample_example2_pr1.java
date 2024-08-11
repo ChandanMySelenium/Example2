466b8646937a8aa57209c0bdd099cda44856368e
DELL <DELL@DESKTOP-TL8GQ8N>
2024-08-11 20:46:22 +0530

    class name changed

--- Example2/src/sampleExample2/sample_example2_pr1.java
+++ Example2/src/sampleExample2/sample_example2_pr1.java
@@ -1,6 +1,6 @@
 package sampleExample2;
 
-public class sample_example2 {
+public class sample_example2_pr1 {
 
 	public static void main(String[] args) {
 		System.out.println("demo");
--- Example2/src/sampleExample2/sample_example2_pr2.java
+++ Example2/src/sampleExample2/sample_example2_pr2.java
@@ -1,6 +1,6 @@
 package sampleExample2;
 
-public class sample_prg2 {
+public class sample_example2_pr2 {
 
 	public static void main(String[] args) {
 		System.out.println("hellow sample prg2");
