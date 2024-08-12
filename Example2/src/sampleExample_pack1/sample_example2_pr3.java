6e73b9f77dd1cb87317279bc1f323f594e68be6d
ChandanMySelenium <chandancontacts2001@gmail.com>
2024-08-12 13:26:32 +0530

    new print statement added in pr3 of pack1

--- Example2/src/sampleExample_pack1/sample_example2_pr3.java
+++ Example2/src/sampleExample_pack1/sample_example2_pr3.java
@@ -3,6 +3,7 @@ package sampleExample_pack1;
 public class sample_example2_pr3 {
 
 	public static void main(String[] args) {
-		System.out.println("hellow sample prg3");
+		System.out.println("hellow sample prg3 test1");
+		System.out.println("hellow sample prg3 test2");
 	}
 }
